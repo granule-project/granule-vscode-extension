c2c4fc1ebc3cfd9544de92c180e8a76ece892aff
decb <decb@users.noreply.github.com>
2020-03-10 12:21:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# granule-vscode-extension
+
+## Commands
+
+- `Ctrl-g Ctrl-r`: attempts to rewrite all non-empty holes in the hole, by
+  case-splitting on the variables they contain
+- `Ctrl-g Ctrl-h`: attempts to rewrite the non-empty hole under the cursor (if
+  there is one)
+- `Ctrl-g Ctrl-u`: converts ASCII characters to their Unicode equivalents
+- `Ctrl-g Ctrl-a`: converts Unicode characters to their ASCII equivalents
+
+The standard undo functionality should work with any of the above commands.
+
+## How to install
+
+```bash
+yarn install
+yarn package
+code --install-extension ./granule-0.0.1.vsix
+```
+
+> Requires `[yarn](https://classic.yarnpkg.com/lang/en/)` and [VSCode](https://code.visualstudio.com/) to be installed
+
+Alternatively, run the first two commands, then add the `.vsix` file to VSCode
+by going to *Extensions*, clicking *...* in the top-right, then *Install from
+VSIX...*.
---
+++ src/extension.ts
@@ -0,0 +1,81 @@
+import * as vscode from "vscode";
+import cp = require("child_process");
+import path = require("path");
+
+export function activate(context: vscode.ExtensionContext) {
+  const outputChannel = vscode.window.createOutputChannel("granule");
+
+  const rewriteHoles = vscode.commands.registerCommand(
+    "granule.rewriteHoles",
+    granuleCommandRunner(outputChannel, "--rewrite-holes")
+  );
+
+  const rewriteHole = vscode.commands.registerCommand(
+    "granule.rewriteHole",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor && editor.selection.isEmpty) {
+        const { character, line } = editor.selection.active;
+        granuleCommandRunner(
+          outputChannel,
+          "--rewrite-holes",
+          "--hole-line",
+          (line + 1).toString(),
+          "--hole-column",
+          (character + 1).toString()
+        )();
+      } else {
+        vscode.window.showWarningMessage("Could not retrieve position under cursor.");
+      }
+    }
+  );
+
+  const asciiToUnicode = vscode.commands.registerCommand(
+    "granule.asciiToUnicode",
+    granuleCommandRunner(outputChannel, "--ascii-to-unicode")
+  );
+
+  const unicodeToAscii = vscode.commands.registerCommand(
+    "granule.unicodeToAscii",
+    granuleCommandRunner(outputChannel, "--unicode-to-ascii")
+  );
+
+  context.subscriptions.push(
+    rewriteHoles,
+    rewriteHole,
+    asciiToUnicode,
+    unicodeToAscii
+  );
+}
+
+export function deactivate() { }
+
+function granuleCommandRunner(outputChannel: vscode.OutputChannel, ...args: string[]): () => void {
+  return () => {
+    const currentDocument = vscode.window.activeTextEditor?.document;
+    if (!currentDocument || currentDocument.isUntitled || currentDocument.isDirty) {
+      vscode.window.showWarningMessage("Please save the file first.");
+    } else if (!currentDocument.fileName.endsWith(".gr")) {
+      vscode.window.showErrorMessage("Command only supported on .gr files.");
+    } else {
+      const filename = currentDocument.fileName;
+      const dirname = path.dirname(filename);
+      const basename = path.basename(filename);
+      const command = args.join(" ");
+
+      const fullCommand = `gr ${command} --no-colour ${basename}`;
+      vscode.window.showInformationMessage(`Running ${fullCommand}`);
+      outputChannel.appendLine(`${dirname} > ${fullCommand}`);
+
+      cp.exec(fullCommand, { cwd: dirname }, (err, stdout, stderr) => {
+        const errString = err?.toString();
+        if (errString && !errString.includes("Type checking failed")) {
+          outputChannel.append(errString);
+        } else {
+          outputChannel.append(stdout);
+          outputChannel.append(stderr);
+        }
+      });
+    }
+  };
+}
